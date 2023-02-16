417fd00c47b6814efb680f50c99612eb25e995bf
itsjenm <jmartinez414@gmail.com>
2023-02-16 05:02:21 -0600

    fixed statements and array mapping

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -5,7 +5,6 @@ const countriesList = document.querySelector(".country-list");
 const countriesInfo = document.querySelector(".country-info");
 
 
-
 //write a function to return an array of countries 
 // Reduce the amount of data transferred by filtering and getting only (name, capital, population, flag svg, languages)
 export default function fetchCountries(name) {
@@ -13,15 +12,15 @@ export default function fetchCountries(name) {
     .then(response => {
         // console.log(response)
         // If the user clears the search box completely, the HTTP request is not executed, and the country list markup or country information disappears.
-        if(name === '') {
+        if(name === '' || !name) {
             countriesList.innerHTML = ""; 
             countriesInfo.innerHTML = "";
+            throw new Error(response.status)
         }
-        // If the user clears the search box completely, the HTTP request is not executed, and the country list markup or country information disappears.
+        // As the user clears their input, clear the dom elements
         if (name.length - 1) {
             countriesInfo.innerHTML = "";
-        }
-
+        } 
 
         // parse data into json
         return response.json()
@@ -39,6 +38,7 @@ export default function fetchCountries(name) {
         
         if (!totalCountries) {
             Notiflix.Notify.failure('Oops, there is no country with that name')
+            countriesList.innerHTML = "";
         }
 
         
@@ -92,7 +92,7 @@ export default function fetchCountries(name) {
             <ul>
                 <li><p>Capital: ${country.capital}</p></li>
                 <li><p>Population: ${country.population}</p></li>
-                <li><p>Languages: ${country.languages[0].name}</p></li>
+                <li><p>Languages: ${country.languages.map(languages => `<ol>${languages.name}</ol>`)}</p></li>
             </ul>
             `
         })
